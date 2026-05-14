b9cc08d06f5d53eec7569385a0fb8c307cbb01af
David Stritzl <437018+davidstritzl@users.noreply.github.com>
2026-05-14 13:32:04 +0200

    avr-gdb: Upgrade to GDB 17.2
    Signed-off-by: David Stritzl <437018+davidstritzl@users.noreply.github.com>

--- Formula/avr-gdb.rb
+++ Formula/avr-gdb.rb
@@ -1,38 +1,41 @@
 class AvrGdb < Formula
   desc "GNU debugger for AVR 8-bit and 32-bit Microcontrollers"
   homepage "https://www.gnu.org/software/gdb/"
-
-  url "https://ftpmirror.gnu.org/gdb/gdb-16.3.tar.xz"
-  mirror "https://ftp.gnu.org/gnu/gdb/gdb-16.3.tar.xz"
-  sha256 "bcfcd095528a987917acf9fff3f1672181694926cc18d609c99d0042c00224c5"
-
+  url "https://ftpmirror.gnu.org/gnu/gdb/gdb-17.2.tar.xz"
+  mirror "https://ftp.gnu.org/gnu/gdb/gdb-17.2.tar.xz"
+  sha256 "1c036c0d72e4b3d1fb5c94c88632add6f9d76f4d7c4d2ea793c12a9f19a3228c"
   license "GPL-3.0-or-later"
-
   head "https://sourceware.org/git/binutils-gdb.git", branch: "master"
 
   livecheck do
     formula "gdb"
   end
 
-  bottle do
-    root_url "https://github.com/osx-cross/homebrew-avr/releases/download/avr-gdb-16.3"
-    sha256 arm64_sequoia: "f5eeb26b1dadd9f23db311aba4d85588da92f15ed61274b6aadcd5e2e7ae02cf"
-    sha256 arm64_sonoma:  "61eb2b3d243f2d6487f2262bd6f4506aa42a8a4571752cf1e460989464a39e76"
-    sha256 ventura:       "ca966c101bdd29ba82ce410b768b2a367812b52b8d5800a44571b4388e54b241"
-  end
-
-  depends_on "avr-gcc@14" => :test
+  depends_on "pkgconf" => :build
+  depends_on "avr-gcc@15" => :test
   depends_on "gmp"
   depends_on "mpfr"
-  depends_on "python@3.12"
+  depends_on "ncurses" # https://github.com/Homebrew/homebrew-core/issues/224294
+  depends_on "python@3.14"
+  depends_on "readline"
   depends_on "xz" # required for lzma support
+  depends_on "zstd"
 
-  uses_from_macos "expat"
-  uses_from_macos "ncurses"
-  uses_from_macos "zlib"
+  uses_from_macos "expat", since: :sequoia # minimum macOS due to python
+
+  # Workaround for https://github.com/Homebrew/brew/issues/19315
+  on_sequoia :or_newer do
+    on_intel do
+      depends_on "expat"
+    end
+  end
 
   on_system :linux, macos: :ventura_or_newer do
     depends_on "texinfo" => :build
+  end
+
+  on_linux do
+    depends_on "zlib-ng-compat"
   end
 
   def install
@@ -43,10 +46,16 @@ class AvrGdb < Formula
       --includedir=#{include}/#{target}
       --infodir=#{info}/#{target}
       --mandir=#{man}
-      --with-lzma
-      --with-python=#{Formula["python@3.12"].opt_bin}/python3.12
-      --with-system-zlib
       --disable-binutils
+      --disable-nls
+      --enable-tui
+      --with-curses
+      --with-expat
+      --with-lzma
+      --with-python=#{which("python3.14")}
+      --with-system-readline
+      --with-system-zlib
+      --with-zstd
     ]
 
     mkdir "build" do
@@ -61,9 +70,8 @@ class AvrGdb < Formula
 
   test do
     (testpath/"test.c").write "void _start(void) {}"
-    system "#{Formula["avr-gcc@14"].bin}/avr-gcc", "-g", "-nostdlib", "test.c"
-
-    output = shell_output("#{bin}/avr-gdb -batch -ex 'info address _start' a.out")
-    assert_match "Symbol \"_start\" is a function at address 0x", output
+    system Formula["avr-gcc@15"].bin/"avr-gcc", "-g", "-nostdlib", "test.c"
+    assert_match "Symbol \"_start\" is a function at address 0x",
+          shell_output("#{bin}/avr-gdb -batch -ex 'info address _start' a.out")
   end
 end
